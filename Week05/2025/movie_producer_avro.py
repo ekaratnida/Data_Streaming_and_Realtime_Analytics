ecd66168cd001881106b1fc2f35c8db22db2a25e
Ekarat <ekarat@as.nida.ac.th>
2026-01-29 13:36:55 +0700

    code

--- Week05/2025/movie_producer_avro.py
+++ Week05/2025/movie_producer_avro.py
@@ -1,14 +1,16 @@
-from confluent_kafka.avro import AvroProducer
-from confluent_kafka.avro import CachedSchemaRegistryClient
+from confluent_kafka import SerializingProducer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
 import random
 from datetime import datetime
 import time
 
-# Set up schema registry client
-schema_registry = CachedSchemaRegistryClient('http://localhost:8081')  # Change this to your Schema Registry URL
+# 1. Setup Schema Registry Client explicitly
+sr_conf = {'url': 'http://localhost:8081'}
+schema_registry_client = SchemaRegistryClient(sr_conf)
 
-# Define the Avro schema
-schema_str = '''
+# 2. Define Schema
+schema_str = """
 {
     "type": "record",
     "name": "TicketSale",
@@ -19,27 +21,23 @@ schema_str = '''
         {"name": "ticket_total_value", "type": "int"}
     ]
 }
-'''
+"""
 
-# Define the Avro producer
-avro_producer = AvroProducer(
-    {
-        'bootstrap.servers': 'localhost:9092',  # Change this to your Kafka broker address
-        'schema.registry.url': 'http://localhost:8081',  # Change this to your Schema Registry URL
-    },
-    default_value_schema=schema_str
-)
+# 3. Create Avro Serializer
+avro_serializer = AvroSerializer(schema_registry_client, schema_str)
+
+# 4. Define Producer with Serializer
+producer_conf = {
+    'bootstrap.servers': 'localhost:9092',
+    'value.serializer': avro_serializer
+}
+producer = SerializingProducer(producer_conf)
 
 topic = 'movie'
-
-# Generate sample records
 titles = ["ET", "Hulk", "Spiderman"]
 prices = [12, 24, 36]
 
-
-# Produce records to Kafka
 for _ in range(5):
-    
     current_time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
     record = {
         'title': random.choice(titles),
@@ -47,8 +45,9 @@ for _ in range(5):
         'ticket_total_value': random.choice(prices),
     }
     
-    avro_producer.produce(topic=topic, value=record)
+    # produce() handles the schema registration automatically here
+    producer.produce(topic=topic, value=record)
     print(f"Produced record: {record}")
     time.sleep(random.randint(1,2))
 
-avro_producer.flush()
+producer.flush()
