1907a820783f3c29a778b3d21625892059d35955
Ekarat <ekarat@as.nida.ac.th>
2026-05-12 11:26:38 +0700

    regressor model

--- Week14/btc.py
+++ Week14/btc.py
@@ -13,22 +13,49 @@ def get_btc_price():
             return None
         
 from river import drift
+from river import time_series, datasets, linear_model
 from time import time, sleep
 
-# Initialize the drift detector
+model = time_series.HoltWinters(
+    alpha=0.3,
+    beta=0.1,
+    gamma=0.6,
+    seasonality=12,
+    multiplicative=True
+)
 
+# Initialize the drift detector
 drift_detector = drift.ADWIN()
 drifts = []
 
+
+#dataset = datasets.AirlinePassengers().take(5)
+#for i, v in dataset:
+#    print(i," ",v)
+
+model = linear_model.LinearRegression(intercept_lr=.1)
+#print(dir(model))
+
 #for i, val in enumerate(stream):
-#for i in range(10):
+#for i in range(5):
+i = 0
 while True:
   #print(f"i={i}, val={val}")
   val = get_btc_price()
   print(val)
+
+  #model.learn_one(val)
+  #print(model.forecast(i))
+  if i<11:
+      print("Learning process...")
+      model.learn_one({i:i},val)
+  else:
+      print(f"Predicted result {model.predict_one({i:i})}")
+ 
   drift_detector.update(val)   # Data is processed one sample at a time
   if drift_detector.drift_detected:
       # The drift detector indicates after each sample if there is a drift in the data
       print(f'Change detected at index {i}')
       drifts.append(i)
-  sleep(10)
+  sleep(5)
+  i=i+1
